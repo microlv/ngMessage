2ae73510e98359ddf2b8de6cad689b4522de7c1b
andy.lv <andy.lv@live.com>
2015-02-17 10:58:53 +0800

    add min

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Andy.Lv on 2015/2/1.
+ */
+'use strict';
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+
+var paths = {
+    scripts: [
+        'ng-message.js'
+    ]
+};
+
+// Not all tasks need to use streams
+// A gulpfile is just another node program and you can use all packages available on npm
+gulp.task('clean', function (cb) {
+    // You can use multiple globbing patterns as you would with `gulp.src`
+    del(['build'], cb);
+});
+
+gulp.task('scripts', ['clean'], function () {
+    // Minify and copy all JavaScript (except vendor scripts)
+    // with sourcemaps all the way down
+    return gulp.src(paths.scripts)
+        .pipe(uglify())
+        .pipe(concat('ng-message.min.js'))
+        .pipe(gulp.dest('.'));
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['scripts']);
